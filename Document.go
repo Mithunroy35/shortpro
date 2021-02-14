b3c58fbe85a08d25e08107919c0fac4e243acbff
“Mithun_roy” <mithun231335@gmail.com>
2021-02-14 23:18:07 +0600

    shortprogramm

---
+++ Document.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var name string = "This is Mithun Roy."
+var name2,city string = "at Agailjhara.", "I live in Barishal"
+
+fmt.Println(name,city,name2)
+
+var c rune = 'A'
+var age int =10
+var m float32 = 50.95
+fmt.Println(c, age, m)
+
+}                                        
