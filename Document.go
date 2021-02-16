f245fec5872507221b3df63a670d721c310243b4
“Mithun_roy” <mithun231335@gmail.com>
2021-02-16 12:10:09 +0600

    change1

--- Document.go
+++ Document.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func main(){
 
+
 var name string = "This is Mithun Roy."
 var name2,city string = "at Agailjhara.", "I live in Barishal"
+var m string = "I am a MBA Student"
 
-fmt.Println(name,city,name2)
+fmt.Println(name,city,name2,m)
 
 var c rune = 'A'
 var age int =10
-var m float32 = 50.95
-fmt.Println(c, age, m)
+var d float32 = 50.95
+
+fmt.Println(c, age, d)
 
 }                                        
